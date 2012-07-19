a63de491cae4929d5dbf46856adf0fd9c85a427d
Daniel Newton <djpnewton@gmail.com>
2012-07-19 20:27:58 +1200

    rearrange so we can call the image stuff in a function

--- research/canfind/dancanfind.py
+++ research/canfind/dancanfind.py
@@ -7,18 +7,8 @@ import math
 RED = 0
 ADAPT = 1
 INDIGO_LASER = 2
-mode = INDIGO_LASER
 
-if __name__ == "__main__":
-
-    # get list of image files
-    if mode == RED:
-        clist = glob.glob("coke*.png")
-    elif mode == ADAPT:
-        clist = glob.glob("coke*.png") + glob.glob("white*.jpg")
-    elif mode == INDIGO_LASER:
-        clist = glob.glob("indigo_laser*.jpg")
-
+def find_can(mode, image):
     # create image buffers
     size = 640, 480
     if mode == RED or mode == INDIGO_LASER:
@@ -46,78 +36,95 @@ if __name__ == "__main__":
         thresholded = cv.CreateImage(size, cv.IPL_DEPTH_8U, 1)
         dilate_eroded = cv.CreateImage(size, cv.IPL_DEPTH_8U, 1)
 
+    # show image
+    cv.ShowImage('cam', image)
+
+    if mode == RED or mode == INDIGO_LASER:
+        # threshold based on HSV values
+        cv.CvtColor(image, hsv_frame, cv.CV_BGR2HSV)
+        cv.InRangeS(hsv_frame, hsv_min, hsv_max, thresholded)
+        cv.InRangeS(hsv_frame, hsv_min2, hsv_max2, thresholded2)
+        cv.Or(thresholded, thresholded2, thresholded)
+
+        # show thresholded image
+        cv.ShowImage('thresh', thresholded)
+
+        # dilate and erode to try to get a solid region of interest
+        cv.Dilate(thresholded, dilate_eroded, None, 20)
+        cv.Erode(dilate_eroded, dilate_eroded, None, 20)
+
+        # smooth image
+        #cv.Smooth(dilate_eroded, dilate_eroded, cv.CV_GAUSSIAN, 9, 9)
+
+        # show dilate and eroded image
+        cv.ShowImage('dilate and erode', dilate_eroded)
+
+        # hough lines 
+        #storage = cv.CreateMemStorage(0)
+        #lines = cv.HoughLines2(thresholded, storage, cv.CV_HOUGH_STANDARD, 1, cv.CV_PI/180, 100, 0, 0)
+        #for (rho, theta) in lines:
+        #    a = math.cos(theta)
+        #    b = math.sin(theta)
+        #    x0 = a * rho
+        #    y0 = b * rho
+        #    pt1 = (cv.Round(x0 + 1000*(-b)), cv.Round(y0 + 1000*(a)))
+        #    pt2 = (cv.Round(x0 - 1000*(-b)), cv.Round(y0 - 1000*(a)))
+        #    cv.Line(image, pt1, pt2, cv.RGB(255, 0, 0), 3, 8)
+        #cv.ShowImage('lines', image)
+
+        final = dilate_eroded
+
+    elif mode == ADAPT:
+        # adaptive threshold
+        cv.CvtColor(image, bw, cv.CV_RGB2GRAY)
+        cv.AdaptiveThreshold(bw, thresholded, 255, cv.CV_ADAPTIVE_THRESH_GAUSSIAN_C, cv.CV_THRESH_BINARY, 31, -10)
+
+        # show thresholded image
+        cv.ShowImage('thresh', thresholded)
+
+        # smooth image
+        cv.Smooth(thresholded, thresholded, cv.CV_GAUSSIAN, 9, 9)
+
+        # show smoothed
+        cv.ShowImage('smoothed', thresholded)
+
+        final = thresholded
+
+    # contours + largest area bounding rect
+    storage = cv.CreateMemStorage(0)
+    contours = cv.FindContours(final, storage)
+    biggest_contours = contours
+    biggest_contours_area = -1
+    while contours:
+        cv.DrawContours(image, contours, cv.RGB(0, 0, 255), cv.RGB(0, 0, 0), 1, 2)
+        area = cv.ContourArea(contours)
+        if area > biggest_contours_area:
+            biggest_contours = contours
+            biggest_contours_area = area
+        contours = contours.h_next()
+    rect = cv.BoundingRect(biggest_contours)
+    cv.Rectangle(image, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), cv.RGB(0, 255, 0))
+    cv.ShowImage('contours', image)
+
+
+if __name__ == "__main__":
+
+    # set mode
+    mode = INDIGO_LASER
+
+    # get list of image files
+    if mode == RED:
+        clist = glob.glob("coke*.png")
+    elif mode == ADAPT:
+        clist = glob.glob("coke*.png") + glob.glob("white*.jpg")
+    elif mode == INDIGO_LASER:
+        clist = glob.glob("indigo_laser*.jpg")
+
     for f in clist:
-        # load and show image
+        # load image
         image = cv.LoadImage(f, cv.CV_LOAD_IMAGE_COLOR)
-        cv.ShowImage('cam', image)
-
-        if mode == RED or mode == INDIGO_LASER:
-            # threshold based on HSV values
-            cv.CvtColor(image, hsv_frame, cv.CV_BGR2HSV)
-            cv.InRangeS(hsv_frame, hsv_min, hsv_max, thresholded)
-            cv.InRangeS(hsv_frame, hsv_min2, hsv_max2, thresholded2)
-            cv.Or(thresholded, thresholded2, thresholded)
-
-            # show thresholded image
-            cv.ShowImage('thresh', thresholded)
-
-            # dilate and erode to try to get a solid region of interest
-            cv.Dilate(thresholded, dilate_eroded, None, 20)
-            cv.Erode(dilate_eroded, dilate_eroded, None, 20)
-
-            # smooth image
-            #cv.Smooth(dilate_eroded, dilate_eroded, cv.CV_GAUSSIAN, 9, 9)
-
-            # show dilate and eroded image
-            cv.ShowImage('dilate and erode', dilate_eroded)
-
-            # hough lines 
-            #storage = cv.CreateMemStorage(0)
-            #lines = cv.HoughLines2(thresholded, storage, cv.CV_HOUGH_STANDARD, 1, cv.CV_PI/180, 100, 0, 0)
-            #for (rho, theta) in lines:
-            #    a = math.cos(theta)
-            #    b = math.sin(theta)
-            #    x0 = a * rho
-            #    y0 = b * rho
-            #    pt1 = (cv.Round(x0 + 1000*(-b)), cv.Round(y0 + 1000*(a)))
-            #    pt2 = (cv.Round(x0 - 1000*(-b)), cv.Round(y0 - 1000*(a)))
-            #    cv.Line(image, pt1, pt2, cv.RGB(255, 0, 0), 3, 8)
-            #cv.ShowImage('lines', image)
-
-            final = dilate_eroded
-
-        elif mode == ADAPT:
-            # adaptive threshold
-            cv.CvtColor(image, bw, cv.CV_RGB2GRAY)
-            cv.AdaptiveThreshold(bw, thresholded, 255, cv.CV_ADAPTIVE_THRESH_GAUSSIAN_C, cv.CV_THRESH_BINARY, 31, -10)
-
-            # show thresholded image
-            cv.ShowImage('thresh', thresholded)
-
-            # smooth image
-            cv.Smooth(thresholded, thresholded, cv.CV_GAUSSIAN, 9, 9)
-
-            # show smoothed
-            cv.ShowImage('smoothed', thresholded)
-
-            final = thresholded
-
-        # contours + largest area bounding rect
-        storage = cv.CreateMemStorage(0)
-        contours = cv.FindContours(final, storage)
-        biggest_contours = contours
-        biggest_contours_area = -1
-        while contours:
-            cv.DrawContours(image, contours, cv.RGB(0, 0, 255), cv.RGB(0, 0, 0), 1, 2)
-            area = cv.ContourArea(contours)
-            if area > biggest_contours_area:
-                biggest_contours = contours
-                biggest_contours_area = area
-            contours = contours.h_next()
-        rect = cv.BoundingRect(biggest_contours)
-        cv.Rectangle(image, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), cv.RGB(0, 255, 0))
-        cv.ShowImage('contours', image)
-        
+        # process image
+        find_can(mode, image);
         # wait for key press before showing next image
         key = cv.WaitKey(-1)
         # if escape pressed then exit
