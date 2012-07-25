a4820935027f226306fff7293908a9523a9b650c
Daniel Newton <djpnewton@gmail.com>
2012-07-25 19:56:35 +1200

    set any program with user button

--- beagleboard_scripts/user_button.py
+++ beagleboard_scripts/user_button.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python
 # Beagleboard user button
-import struct
-inputDevice = "/dev/input/event1"
-# format of the event structure (int, int, short, short, int)
-inputEventFormat = 'iihhi'
-inputEventSize = 16
- 
-file = open(inputDevice, "rb") # standard binary file input
-event = file.read(inputEventSize)
-state = 0
-while event:
-  (time1, time2, type, code, value) = struct.unpack(inputEventFormat, event)
-  if type == 1 and code == 276 and value == 1:
-    import os
-    if state % 2 == 0:
-      print "start!", state
-      os.system("python /home/root/teensy/controller.py 1")
-      os.system("python /home/root/teensy/controller.py 3")
-    else:
-      print "stop!", state
-      os.system("python /home/root/teensy/controller.py 0")
-    state = state + 1
-  event = file.read(inputEventSize)
-file.close()
+import os
+
+CONTROLLER_PATH = "python /home/root/teensy/controller.py"
+counter = 0
+
+def start_controller():
+    global counter
+    print "start controller %d!" % (counter - 1)
+    os.system("%s -p%d" % (CONTROLLER_PATH, counter - 1))
+
+    counter = 0
+
+def read_button():
+    import threading
+    import struct
+    inputDevice = "/home/root/gpio-keys-event"
+    # format of the event structure (int, int, short, short, int)
+    inputEventFormat = 'iihhi'
+    inputEventSize = 16
+
+    file = open(inputDevice, "rb") # standard binary file input
+    event = file.read(inputEventSize)
+    timer = None
+    while event:
+        (time1, time2, type, code, value) = struct.unpack(inputEventFormat, event)
+        if type == 1 and code == 276 and value == 1:
+            global counter
+            counter += 1
+            print "user button", counter
+            if timer:
+                timer.cancel()
+            timer = threading.Timer(2, start_controller)
+            timer.start()
+            os.system("%s -f%d -t%d" % (CONTROLLER_PATH, 50 + counter * 10, 3000))
+        event = file.read(inputEventSize)
+    file.close()
+
+if __name__ == "__main__":
+    read_button()
--- beagleboard_scripts/user_button.sh
+++ beagleboard_scripts/user_button.sh
@@ -3,4 +3,8 @@
 LD_LIBRARY_PATH=/home/root/teensy
 export LD_LIBRARY_PATH
 modprobe gpio-keys
+INPUT_DEV=`cat /proc/bus/input/devices | grep "gpio-keys" | grep "Sysfs" | sed 's/.*\/input\(\)/\1/'`
+INPUT_DEVNODE=/dev/input/event$INPUT_DEV
+rm /home/root/gpio-keys-event
+ln -s $INPUT_DEVNODE /home/root/gpio-keys-event
 python /home/root/user_button.py &
