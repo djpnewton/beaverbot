aef28d78b1c6e92de4d25e621668b985332439b4
Daniel Newton <djpnewton@gmail.com>
2012-07-26 18:45:06 +1200

    correct setup_webcam.sh script for usage in boot process

--- beagleboard_scripts/setup_webcam.sh
+++ beagleboard_scripts/setup_webcam.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=.
-./uvcdynctrl -L lifecam_hd5000_maxwhitebalance.gpfl
+export LD_LIBRARY_PATH=/home/root
+/home/root/uvcdynctrl -L /home/root/lifecam_hd5000_maxwhitebalance.gpfl
